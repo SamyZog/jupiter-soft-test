7c5909026e308f64b970b8b9657879e5e724adfc
Samy Zogeyb <samyzogeyb@gmail.com>
2022-06-23 22:18:46 +0400

    add padding to hero section

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,7 +1,7 @@
 import { heroSection } from "../../stubs";
 
 const Hero = () => (
-  <section className="D text-white bg-slate-800">
+  <section className="D text-white bg-slate-800 px-6">
     <div className="flex flex-col space-y-10 justify-center items-center h-72 text-center">
       <h1 className="text-7xl">{heroSection.title}</h1>
       <p className="text-slate-400 max-w-[570px]">{heroSection.text}</p>
